0bd2f596256ca28e93887de2bb0251c795f89f1c
Drew <abemery@clemson.edu>
2021-09-06 14:01:40 -0700

    heroku not building properly

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const onRedirectCallback = (appState) => {
 // Please see https://auth0.github.io/auth0-react/interfaces/auth0_provider.auth0provideroptions.html
 // for a full list of the available properties on the provider
 // const config = getConfig();
-
+console.log({"DOMAIN":process.env.AUTH0_DOMAIN});
 const providerConfig = {
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
